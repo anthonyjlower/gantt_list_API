bf67a41458500b46288d071e497d885c8479c855
Anthony <anthonyjlower@gmail.com>
2018-02-09 12:10:09 -0600

    Initial CRUD routes work for users

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -1,7 +1,65 @@
 class UserController < ApplicationController
 
 	get "/" do
-		"Linked through user controller"
+		@users = User.all
+		resp = {
+			status: {
+				success: true,
+				message: "These are all of the users in the database",
+				number_of_results: @users.length
+			},
+			users: @users
+		}
+		resp.to_json
+	end
+
+
+
+	post "/" do
+		@user = User.new
+		@user.name = params[:name]
+		@user.permission = params[:permission]
+		@user.save
+		resp = {
+			status: {
+				success: true,
+				message: "Account created for #{@user.name}"
+			},
+			user: @user
+		}
+		resp.to_json
+	end
+
+
+	put "/:id" do
+		@user = User.find params[:id]
+		@user.name = params[:name]
+		@user.permission = params[:permission]
+		@user.save
+		resp = {
+			status: {
+				success: true,
+				message: "Account udpated for #{@user.name}"
+			},
+			user: @user
+		}
+		resp.to_json
+	end
+
+
+
+	delete "/:id" do
+		@user = User.find params[:id]
+		@user.delete
+		@users = User.all
+		resp = {
+			status: {
+				success: true,
+				message: "Account has been deleted, this is all of the remaining users"
+			},
+			users: @users
+		}
+		resp.to_json
 	end
 
 end
