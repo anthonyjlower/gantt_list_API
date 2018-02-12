07ed0a3d4b5c8f2b5485ee638c8c4700ca60bf86
Anthony <anthonyjlower@gmail.com>
2018-02-12 13:30:13 -0600

    Changed SQL column headers to match vis.js naming

--- db/migrations.sql
+++ db/migrations.sql
@@ -9,24 +9,24 @@ CREATE TYPE completedBool AS ENUM ('true', 'false');
 
 CREATE TABLE users(
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(255),
+	content VARCHAR(255),
 	permission caneditBool
 );
 
 CREATE TABLE projects(
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(255),
-	startdate DATE,
-	enddate DATE,
+	content VARCHAR(255),
+	start DATE,
+	"end" DATE,
 	completed completedBool,
 	user_id INT references users(id)
 );
 
 CREATE TABLE tasks(
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(255),
-	startdate DATE,
-	enddate DATE,
+	content VARCHAR(255),
+	start DATE,
+	"end" DATE,
 	completed completedBool,
 	belongs_to INT, -- 0 if it is top level, otherwise it references the task it is a sub of
 	project_id INT references projects(id)
