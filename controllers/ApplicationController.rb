c8ce011a7da350f455b1f61154a0820e98950dac
Anthony <anthonyjlower@gmail.com>
2018-02-16 10:38:59 -0600

    Prepared heroku

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -2,16 +2,13 @@ class ApplicationController < Sinatra::Base
 
 	require "bundler"
 	Bundler.require();
+	require './config/environments'
+	
 	enable :sessions
 	set :views, File.expand_path("../../views", __FILE__)
 	set :public_dir, File.expand_path("../../public", __FILE__)
 	
 	register Sinatra::CrossOrigin
-	
-	ActiveRecord::Base.establish_connection(
- 		:adapter => 'postgresql', 
- 		:database => 'gantt'
-	)
 
 	use Rack::MethodOverride  # we "use" middleware in Rack-based libraries/frameworks
 	set :method_override, true
